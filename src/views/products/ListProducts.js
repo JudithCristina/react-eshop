b6ca9ca9814f017e75064dae08031733fedd48a0
Judith Cristina Quiñones Inga <judicriss18@gmail.com>
2021-06-19 11:48:35 -0500

    lista de productos

--- src/views/products/ListProducts.js
+++ src/views/products/ListProducts.js
@@ -1,9 +1,108 @@
-import React from 'react'
-
+import React from 'react';
+import { CardColumns, Card, Button } from "react-bootstrap";
+import "./ListProducts.css"
 const ListProducts = () => {
   return (
-    <div>
-      <p>Lista de productos</p>
+    <div className="container-list-products">
+      <CardColumns>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to additional
+              content.{' '}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div className="product-card-footer">
+              <p> S/.17</p>
+              <Button variant="primary">Go somewhere</Button>
+            </div>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to additional
+              content.{' '}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div className="product-card-footer">
+              <p> S/.17</p>
+              <Button variant="primary">Go somewhere</Button>
+            </div>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to additional
+              content.{' '}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div className="product-card-footer">
+              <p> S/.17</p>
+              <Button variant="primary">Go somewhere</Button>
+            </div>
+          </Card.Footer>
+        </Card>
+         <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to additional
+              content.{' '}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div className="product-card-footer">
+              <p> S/.17</p>
+              <Button variant="primary">Go somewhere</Button>
+            </div>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to additional
+              content.{' '}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div className="product-card-footer">
+              <p> S/.17</p>
+              <Button variant="primary">Go somewhere</Button>
+            </div>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to additional
+              content.{' '}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <div className="product-card-footer">
+              <p> S/.17</p>
+              <Button variant="primary">Go somewhere</Button>
+            </div>
+          </Card.Footer>
+        </Card>
+
+      </CardColumns>
     </div>
   )
 }
